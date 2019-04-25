167a850f7ac176b730402d52ee9b144478b2a3c4
Canplayer <chenchi98@live.com>
2019-04-25 18:59:44 +0800

    remove password

--- index.js
+++ index.js
@@ -1,76 +1,54 @@
 const WebSocket = require('ws')
 const ws = new WebSocket.Server({ port: 56789 })
 const fs = require('fs')
-const registerDatabaseName = './registerDatabaseName.json'
-let registerDatabase = {
-}
-
-fs.exists(registerDatabaseName, (exists) => {
-    if (exists) {
-        fs.readFile(registerDatabaseName, (err, data) => {
-            registerDatabase = JSON.parse(data.toString())
-            startServer()
-        })
-    } else {
-        fs.writeFile(registerDatabaseName, JSON.stringify(registerDatabase), (err) => {
-            startServer()
-        })
-    }
-})
-let loggedUser = []
+let registerDatabase = []
+let loggedUser = {}
 function startServer() {
     ws.on('connection', (ws) => {
         ws.on('message', (message) => {
-            console.log(message)
             try {
                 let data = JSON.parse(message)
-                console.log(data)
                 if (data.type != null) {
                     if (data.type === 1) {
                         let userName = data.userName;
                         let content = data.content;
-                        loggedUser.forEach(item => {
-                            item.send(JSON.stringify({
-                                type: 1,
-                                content: content,
-                                userName: userName
-                            }))
+                        console.log(`message received: ${message}`)
+                        Object.keys(loggedUser).forEach((name) => {
+                            if (name !== userName) {
+                                loggedUser[name].send(JSON.stringify({
+                                    type: 1,
+                                    content: content,
+                                    userName: userName
+                                }))
+                            }
                         })
                     } else if (data.type === 0) {
                         let userName = data.userName;
-                        let password = data.password;
-                        if (registerDatabase[userName] && registerDatabase[userName] === password) {
-                            ws.send(JSON.stringify({
-                                type: 3,
-                                success: true
-                            }));
-                            console.log(`User Logged ${userName}`)
-                            loggedUser.push(ws)
-                        } else {
+                        if (registerDatabase.indexOf(userName) !== -1) {
                             ws.send(JSON.stringify({
                                 type: 3,
                                 success: false
                             }));
-                        }
-                    } else if (data.type === 2) {
-                        console.log('sign up')
-                        let userName = data.userName;
-                        let password = data.password;
-                        if (registerDatabase[userName]) {
-                            // reply existed
-                            ws.send('userName existed')
                         } else {
-                            registerDatabase[userName] = password
-                            fs.writeFile(registerDatabaseName, JSON.stringify(registerDatabase), (err) => {
-                                // reply success
-                                ws.send('user created')
+                            ws.send(JSON.stringify({
+                                type: 3,
+                                success: true
+                            }));
+                            console.log(`User Logged: ${userName}`)
+                            registerDatabase.push(userName)
+                            loggedUser[userName] = ws
+                            ws.on('close', () => {
+                                console.log(`User Disconnected: ${userName}`)
+                                let index = registerDatabase.indexOf(userName)
+                                registerDatabase.splice(index, 1)
                             })
                         }
                     }
                 }
             } catch{
-
+                console.log(`corrupted message: ${message}`)
             }
         })
     })
 }
+startServer()
